d545b79a9d71b457ccd128e13180bf555ee267d6
LimMinKyo <story1400@naver.com>
2022-03-15 00:24:52 +0900

    Setup

---
+++ src/Router.tsx
@@ -0,0 +1,14 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Home from './routes/Home';
+
+function Router () {
+  return (
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
